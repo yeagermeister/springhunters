e4dc2d7918775709edb323147ca3717d55d5f325
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2022-12-15 15:16:40 -0500

    left some stuff commented, added basic maps functionality, currently broken code

---
+++ Assets/js/script.js
@@ -0,0 +1,13 @@
+let map = new google.maps.Map(document.getElementById("map"))
+
+
+
+function initMap(){
+    var uluru = s
+    {
+        var options = {
+            zoom: 8,
+            center: {lat: 28.7302, lng: 81.2023 }
+        }
+    }
+}
