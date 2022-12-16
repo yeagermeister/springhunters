5d43befe636d8f74b2b10a052deb57be732b390e
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2022-12-15 19:48:08 -0500

    faulty code deleted

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,3 @@
-function initMap(){
-    {
-        //your prime gator location
-        var lakeJesup = {lat: 28.7302, lng: 81.2023 }
-        // the map, centered on everyones favorite lake
-        let map = new google.maps.Map(document.getElementById("map"),{
-            zoom: 8,
-            center: lakeJesup,
-        
-        })
-    }
-}
 
 // Initialize the map when the page is loaded
 window.onload = function() {
