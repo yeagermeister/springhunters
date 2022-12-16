8093b8fba7f0b6897c33bd268b273b551705a0eb
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2022-12-15 19:30:11 -0500

    small change
    test

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,12 @@
-let map = new google.maps.Map(document.getElementById("map"))
-
-
-
-function initMap(){
-    var uluru = s
-    {
-        var options = {
-            zoom: 8,
-            center: {lat: 28.7302, lng: 81.2023 }
-        }
-    }
-}
+// function initMap(){
+//     {
+//         //your prime gator location
+//         var lakeJesup = {lat: 28.7302, lng: 81.2023 }
+//         // the map, centered on everyones favorite lake
+//         let map = new google.maps.Map(document.getElementById("map"),{
+//             zoom: 8,
+//             center: lakeJesup,
+        
+//         })
+//     }
+// }
