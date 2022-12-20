df1959e5095ddf13a21509ff4cb6dacc92ac2799
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2022-12-19 20:37:44 -0500

    we have a working mapgit add -A

--- Assets/js/googleapi.js
+++ Assets/js/googleapi.js
@@ -1,21 +1,77 @@
 // Initialize and add the map
+var mapId = document.getElementById("map");
+const latitude = 28.6568;
+const longitude = -81.2436;
 const API_KEY = 'AIzaSyAUPFIpucG-X584hME5DFs-4Yu28ny2vVk';
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
+// function initMap() {
+    
+//     var map = new google.maps.Map(mapId, {
+//   center: { lat: "-34.397", lng:" 150.644" },
+//   zoom: 8,
+  
+// });
+// // console.log(mapId)
+// // console.log(google)
+// // console.log(google.maps)
+// // console.log(google.maps.Map)
+// }
+  
+// window.onload = function() {
+//     initMap();
+// // window.onload = function() {
+//     const map = new google.maps.Map(mapId, mapOptions);
+//   }
+
+window.onload = function() {
+    initMap();
+  };
+  // Initialize the map
+  function initMap() {
+    // Set up the map options
+    const mapOptions = {
+      zoom: 10,
+      center: new google.maps.LatLng(latitude, longitude)
+    };
+    // Create the map
+    const map = new google.maps.Map(mapId, mapOptions);
+    // Add a marker to the map at the specified coordinates
     const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
+      position: {lat: latitude, lng: longitude},
+      map: map
     });
   }
   
-  window.onload() = initMap();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  window.onload = function() {initMap};
+//   <script async
+//     src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places&callback=initMap">
+// </script>
 
 
   
