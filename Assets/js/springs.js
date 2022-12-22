b5c1e79910e1f1ce944ef68ece79f2688af1709a
unknown <johnyeager75@gmail.com>
2022-12-22 10:13:14 -0500

    update list in the slack channel

--- Assets/js/springs.js
+++ Assets/js/springs.js
@@ -1,24 +1,24 @@
-var dropdownEl = document.querySelector("#dropdown");
-var parknameEl = document.querySelector("#picture");
-var descriptionEl = document.querySelector("#picture");
-var distanceEl = document.querySelector("#picture");
-var admissionEl = document.querySelector("#picture");
-var petsEl = document.querySelector("#picture");
-var stateparkEl = document.querySelector("#picture");
-var gatorEl = document.querySelector("#picture");
-var scubaEl = document.querySelector("#picture");
-var ratingEl = document.querySelector("#picture");
-var noteEl = document.querySelector("#picture");
+let dropdownEl = document.querySelector("#dropdown");
+let parknameEl = document.querySelector("#parkname");
+let descriptionEl = document.querySelector("#description");
+let distanceEl = document.querySelector("#distance");
+let admissionEl = document.querySelector("#admission");
+let petsEl = document.querySelector("#pets");
+let stateparkEl = document.querySelector("#statepark");
+let campingEl = document.querySelector("#camping");
+let gatorEl = document.querySelector("#gator");
+let scubaEl = document.querySelector("#scuba");
+let ratingEl = document.querySelector("#rating");
+let noteEl = document.querySelector("#note");
 
-var dropdownList = ["Wekiwa Springs State Park", "Silver Springs State Park", "Rainbow Springs State Park", "Rock Springs Run State Reserve", "Ginnie Springs", "Blue Spring State Park", "DeLeon Springs State Park", "Fanning Springs State Park", "Manatee Springs State Park", "Weeki Wachee Springs State Park", "Ichetucknee Srings State Park", "Weeki Wachee Springs State Park", "Royal Springs", "Bob's River Place"];
+let dropdownList = ["Wekiwa Springs State Park", "Silver Springs State Park", "Rainbow Springs State Park", "Rock Springs Run State Reserve", "Ginnie Springs", "Blue Spring State Park", "DeLeon Springs State Park", "Fanning Springs State Park", "Manatee Springs State Park", "Weeki Wachee Springs State Park", "Ichetucknee Srings State Park", "Weeki Wachee Springs State Park", "Royal Springs", "Bob's River Place"];
 
-function init(){
-    populateDropdown();
-};
 
+// This will run on page load to populate the drop dow list
 function populateDropdown() {
     for (let i = 1; i < dropdownList.length; i++) {
-      var optionEl = document.createElement('option');
+      let optionEl = document.createElement('option');
+      // we may not need these next 2 lines
       optionEl.setAttribute('id', 'option-' + i);
       optionEl.setAttribute('index', i);
       optionEl.textContent = dropdownList[i];
@@ -26,16 +26,40 @@ function populateDropdown() {
     }
 };
 
-dropdownEl.addEventListener("click", function(event) {
-  // var element = event.target;
+function populateParkInfo(park) {
+  console.log(park.name);
+  parknameEl.textContent = park.name;
+  descriptionEl.textContent = park.description;
+  distanceEl.textContent = "miles";
+  admissionEl.textContent = park.fees;
+  if (park.pets) {
+    petsEl.textContent = "Pet Friendly"
+  } else (petsEl.textContent = "Pet's not allowed");
+  if (park.statepark) {
+    stateparkEl.textContent = "This is a state park"
+  } else (stateparkEl.textContent = "This is not a state park");
+  if (park.gatordanger) {
+    gatorEl.textContent = "Beware of the gators"
+  } else (gatorEl.textContent = "No Gator Danger");
+  if (park.camping) {
+    campingEl.textContent = "Camping is Allowed"
+  } else (campingEl.textContent = "No Camping");
+  if (park.scuba) {
+    scubaEl.textContent = "Scuba Diving Allowed"
+  } else (scubaEl.textContent = "No Scuba Diving");
 
-  var select = document.getElementById("dropdown");
-  var value = select.options[select.selectedIndex].value;
-  console.log(value)
-  const found = dropdownList.findIndex(element => element = value)
-  console.log(found)
+  // Need to implement local storage for these to work
+  // ratingEl.textContent = 
+  // noteEl.textContent = 
+};
 
+
+// Get the correct park array from session storage when the drop down list is used
+$("#dropdown").on("change", function() {
+  let value = dropdownEl.options[dropdownEl.selectedIndex].value;
+  let park = JSON.parse(sessionStorage.getItem(value));
+  populateParkInfo(park);
 });
 
-init();
+populateDropdown();
 
